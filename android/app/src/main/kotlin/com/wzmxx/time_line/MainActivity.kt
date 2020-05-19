65c8cb0873f432a1c7a30174f40efc511f1abe3f
朱星杰 <1290279000@qq.com>
2020-05-19 21:49:50 +0800

    初次提交

---
+++ android/app/src/main/kotlin/com/wzmxx/time_line/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.wzmxx.time_line
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
